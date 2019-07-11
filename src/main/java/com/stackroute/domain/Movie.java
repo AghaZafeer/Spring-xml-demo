3cf5c60bda28624f30a6d040a5dadcb17a0990b7
AghaZafeer <agha.zafeer@gmail.com>
2019-07-11 21:29:20 +0530

    task1

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,20 @@
+package com.stackroute.domain;
+
+public class Movie {
+     private Actor actor;
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+/*  public Movie(Actor actor1, Actor actor2) {
+        this.actor1 = actor1;
+        this.actor2 = actor2;
+    }*/
+
+    public void printAboutMovieActor()
+    {
+        actor.printActor();
+
+    }
+}
