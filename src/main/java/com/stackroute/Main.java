d3b9ce80a9ac54c8930ae45c16bbaa7b749c668f
AghaZafeer <agha.zafeer@gmail.com>
2019-07-11 16:41:42 +0530

    dd

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,15 @@
+package com.stackroute;
+
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie=context.getBean("movie",Movie.class);
+        movie.printAboutMovieActor();
+    }
+}
