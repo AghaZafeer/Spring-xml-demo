29c7b155f9a09456e9d474c4b116f7b1c47043c4
AghaZafeer <51943287+AghaZafeer@users.noreply.github.com>
2019-07-19 10:41:05 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,14 +1,15 @@
 package com.stackroute.domain;
 
 public class Movie {
+     //Actor object as an attribute of Movie class
      Actor actor;
 
-    public void setActor(Actor actor) {
+    public void setActor(Actor actor) {//Setting an actor object
         this.actor = actor;
     }
 
     public void printAboutMovieActor()
     {
-        actor.printActor();
+        actor.printActor();//Printing Actor description
     }
 }
