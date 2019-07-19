75c073e7f1bafe8cd3f72b6168839dc491ef0748
AghaZafeer <51943287+AghaZafeer@users.noreply.github.com>
2019-07-19 10:37:50 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,14 +8,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+        //Bean container creation using ApplicationContext
         ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
         Movie movie=context.getBean("movie",Movie.class);
         movie.printAboutMovieActor();
         
+            //Bean container creation using BeanFactory
          BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
         Movie mv1= (Movie) beanFactory.getBean("movie");
         mv1.printAboutMovieActor();
-
+        
+            //Bean container creation using BeanDefinitionRegistry
         BeanDefinitionRegistry factory = new XmlBeanFactory(new ClassPathResource("beans.xml));
         XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
         Movie mv2 =((XmlBeanFactory)factory).getBean("movie",Movie.class);
