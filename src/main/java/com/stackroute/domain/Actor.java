2f81062d7d335cc527e38915d5916714c16da5c2
AghaZafeer <51943287+AghaZafeer@users.noreply.github.com>
2019-07-19 10:39:41 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,25 +1,26 @@
 package com.stackroute.domain;
 
 public class Actor {
+    //Actor fields
     private String name;
     private String gender;
     private int age;
 
 
-    public void setName(String name) {
+    public void setName(String name) {//Setter for name
         this.name = name;
     }
 
-    public void setGender(String gender) {
+    public void setGender(String gender) {//Setter for gender
         this.gender = gender;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age) {//Setter for age
         this.age = age;
     }
 
     public void printActor()
-    {
+    {   //Printing Actor description
         System.out.println("Actor's Name:"+name+"\n"+"Gender:"+gender+"\n"+"Age:"+age);
     }
 
